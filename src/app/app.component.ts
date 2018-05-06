7d552461f1d30bc48df7b0d0b477624c5bf55aa3
Idirabdenour <idir.abdenour@hotmail.com>
2018-05-06 23:17:08 +0200

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+     // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCrR0q6Ott1vuG_HtkIxtDlN_U598riKZU",
+    authDomain: "database-6ba15.firebaseapp.com",
+    databaseURL: "https://database-6ba15.firebaseio.com",
+    projectId: "database-6ba15",
+    storageBucket: "database-6ba15.appspot.com",
+    messagingSenderId: "603172313635"
+  };
+  firebase.initializeApp(config);
+  }
 }
